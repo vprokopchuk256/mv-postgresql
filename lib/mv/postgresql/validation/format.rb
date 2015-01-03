883bdfc2027afc2b7bb3659f8024f97d84a3ade5
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-03 12:47:38 +0200

    format validation is moved from core

--- lib/mv/postgresql/validation/format.rb
+++ lib/mv/postgresql/validation/format.rb
@@ -1,9 +1,19 @@
 module Mv
   module Postgresql
     module Validation
-      class Format < Mv::Core::Validation::Format
+      class Format < Mv::Core::Validation::Base
+        attr_reader :with
+
+        validates :with, presence: true
+
         def initialize(table_name, column_name, opts)
-          super
+          super(table_name, column_name, opts)
+
+          @with = opts.with_indifferent_access[:with]
+        end
+
+        def to_a
+          super + [with.to_s]
         end
 
         protected 
---
+++ spec/mv/postgresql/validation/factory_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+require 'mv/core/validation/factory'
+
+describe Mv::Core::Validation::Factory do
+  subject(:factory) { described_class }
+
+  describe "exclusion" do
+    subject { factory.create_validation(:table_name, 
+                                        :column_name, 
+                                        :exclusion, 
+                                        { as: :check })}
+
+    it { is_expected.to be_kind_of(Mv::Postgresql::Validation::Exclusion) }
+  end
+
+  describe "uniqueness" do
+    subject { factory.create_validation(:table_name, 
+                                        :column_name, 
+                                        :uniqueness, 
+                                        { as: :check })}
+
+    it { is_expected.to be_kind_of(Mv::Core::Validation::Uniqueness) }
+  end
+
+  describe "format" do
+    subject { factory.create_validation(:table_name, 
+                                        :column_name, 
+                                        :format, 
+                                        { as: :check })}
+
+    it { is_expected.to be_kind_of(Mv::Postgresql::Validation::Format) }
+  end
+
+  describe "inclusion" do
+    subject { factory.create_validation(:table_name, 
+                                        :column_name, 
+                                        :inclusion, 
+                                        { as: :check })}
+
+    it { is_expected.to be_kind_of(Mv::Postgresql::Validation::Inclusion) }
+  end
+
+  describe "length" do
+    subject { factory.create_validation(:table_name, 
+                                        :column_name, 
+                                        :length, 
+                                        { as: :check })}
+
+    it { is_expected.to be_kind_of(Mv::Core::Validation::Length) }
+  end
+
+  describe "presence" do
+    subject { factory.create_validation(:table_name, 
+                                        :column_name, 
+                                        :presence, 
+                                        { as: :check })}
+
+    it { is_expected.to be_kind_of(Mv::Postgresql::Validation::Presence) }
+  end
+end
