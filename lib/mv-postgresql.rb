e6a4ae7540a56ee541a4bf67389080d276a300c8
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-08 21:34:48 +0200

    trigger specific validation builders are registered

--- lib/mv-postgresql.rb
+++ lib/mv-postgresql.rb
@@ -13,6 +13,13 @@ require 'mv/postgresql/validation/inclusion'
 require 'mv/postgresql/validation/length'
 require 'mv/postgresql/validation/presence'
 
+require 'mv/postgresql/validation/builder/trigger/exclusion'
+require 'mv/postgresql/validation/builder/trigger/inclusion'
+require 'mv/postgresql/validation/builder/trigger/length'
+require 'mv/postgresql/validation/builder/trigger/format'
+require 'mv/postgresql/validation/builder/trigger/presence'
+require 'mv/postgresql/validation/builder/trigger/uniqueness'
+
 ActiveSupport.on_load(:mv_core) do
   #router
   Mv::Core::Router.define_route(:check, Mv::Postgresql::Route::Check)
@@ -30,5 +37,33 @@ ActiveSupport.on_load(:mv_core) do
   Mv::Core::Validation::Factory.register_validation(:inclusion, Mv::Postgresql::Validation::Inclusion)
   Mv::Core::Validation::Factory.register_validation(:length, Mv::Postgresql::Validation::Length)
   Mv::Core::Validation::Factory.register_validation(:presence, Mv::Postgresql::Validation::Presence)
+
+  #validation builders in trigger
+  Mv::Postgresql::Constraint::Builder::Trigger.validation_builders_factory.register_builder(
+    Mv::Postgresql::Validation::Exclusion, 
+    Mv::Postgresql::Validation::Builder::Trigger::Exclusion
+  )
+  Mv::Postgresql::Constraint::Builder::Trigger.validation_builders_factory.register_builder(
+    Mv::Postgresql::Validation::Inclusion, 
+    Mv::Postgresql::Validation::Builder::Trigger::Inclusion
+  )
+  Mv::Postgresql::Constraint::Builder::Trigger.validation_builders_factory.register_builder(
+    Mv::Postgresql::Validation::Length, 
+    Mv::Postgresql::Validation::Builder::Trigger::Length
+  )
+  Mv::Postgresql::Constraint::Builder::Trigger.validation_builders_factory.register_builder(
+    Mv::Postgresql::Validation::Format, 
+    Mv::Postgresql::Validation::Builder::Trigger::Format
+  )
+  Mv::Postgresql::Constraint::Builder::Trigger.validation_builders_factory.register_builder(
+    Mv::Postgresql::Validation::Presence, 
+    Mv::Postgresql::Validation::Builder::Trigger::Presence
+  )
+  Mv::Postgresql::Constraint::Builder::Trigger.validation_builders_factory.register_builder(
+    Mv::Core::Validation::Uniqueness, 
+    Mv::Postgresql::Validation::Builder::Trigger::Uniqueness
+  )
+
+
 end
 
--- spec/mv/postgresql/constraint/builder/trigger_spec.rb
+++ spec/mv/postgresql/constraint/builder/trigger_spec.rb
@@ -3,4 +3,94 @@ require 'spec_helper'
 require 'mv/postgresql/constraint/builder/trigger'
 
 describe Mv::Postgresql::Constraint::Builder::Trigger do
+  before do
+    Mv::Core::Services::CreateMigrationValidatorsTable.new.execute
+    Mv::Core::Db::MigrationValidator.delete_all
+  end
+
+  let(:trigger_description) { Mv::Core::Constraint::Description.new(:idx_mv_table_name, :trigger) }
+  let(:trigger_constraint) { Mv::Core::Constraint::Trigger.new(trigger_description) }
+
+  subject(:trigger_builder) { described_class.new(trigger_constraint) }
+
+  describe "#validation_builders" do
+    subject { trigger_builder.validation_builders }
+
+    before do
+      trigger_constraint.validations << validation
+    end
+
+    describe "when exlusion validation provided" do
+      let(:validation) {
+        Mv::Postgresql::Validation::Exclusion.new(:table_name, 
+                                                 :column_name, 
+                                                 in: [1, 3],
+                                                 as: :trigger, 
+                                                 update_trigger_name: :trg_mv_table_name) 
+      }
+
+      its(:first) { is_expected.to be_a_kind_of(Mv::Postgresql::Validation::Builder::Trigger::Exclusion) }
+    end
+
+    describe "when inclusion validation provided" do
+      let(:validation) {
+        Mv::Postgresql::Validation::Inclusion.new(:table_name, 
+                                                 :column_name, 
+                                                 in: [1, 3],
+                                                 as: :trigger, 
+                                                 update_trigger_name: :trg_mv_table_name) 
+      }
+
+      its(:first) { is_expected.to be_a_kind_of(Mv::Postgresql::Validation::Builder::Trigger::Inclusion) }
+    end
+
+    describe "when length validation provided" do
+      let(:validation) {
+        Mv::Postgresql::Validation::Length.new(:table_name, 
+                                                 :column_name, 
+                                                 in: [1, 3],
+                                                 as: :trigger, 
+                                                 update_trigger_name: :trg_mv_table_name) 
+      }
+
+      its(:first) { is_expected.to be_a_kind_of(Mv::Postgresql::Validation::Builder::Trigger::Length) }
+    end
+
+    describe "when format validation provided" do
+      let(:validation) {
+        Mv::Postgresql::Validation::Format.new(:table_name, 
+                                               :column_name, 
+                                               with: /exp/,
+                                               as: :trigger, 
+                                               update_trigger_name: :trg_mv_table_name) 
+      }
+
+      its(:first) { is_expected.to be_a_kind_of(Mv::Postgresql::Validation::Builder::Trigger::Format) }
+    end
+
+    describe "when presence validation provided" do
+      let(:validation) {
+        Mv::Postgresql::Validation::Presence.new(:table_name, 
+                                               :column_name, 
+                                               with: /exp/,
+                                               as: :trigger, 
+                                               update_trigger_name: :trg_mv_table_name) 
+      }
+
+      its(:first) { is_expected.to be_a_kind_of(Mv::Postgresql::Validation::Builder::Trigger::Presence) }
+    end
+
+    describe "when uniqueness validation provided" do
+      let(:validation) {
+        Mv::Core::Validation::Uniqueness.new(:table_name, 
+                                               :column_name, 
+                                               with: /exp/,
+                                               as: :trigger, 
+                                               update_trigger_name: :trg_mv_table_name) 
+      }
+
+      its(:first) { is_expected.to be_a_kind_of(Mv::Postgresql::Validation::Builder::Trigger::Uniqueness) }
+    end
+  end
+
 end
